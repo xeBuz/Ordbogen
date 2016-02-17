a679dcffaefea31428fddbb4ea3cbf831aff00c0
Jesus Roldan <xebuz18@gmail.com>
2016-02-17 01:14:36 -0300

    Better DB path handling

--- config.py
+++ config.py
@@ -1,23 +1,22 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-database_path = "/database/"
+database_path = basedir + "/database/"
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class Development(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir + database_path, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(database_path, 'app.db')
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
 class Production(Config):
-    pass
+    DEBUG = False
+    TESTING = False
 
 
 class Testing(Config):
